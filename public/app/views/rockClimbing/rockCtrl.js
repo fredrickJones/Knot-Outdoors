f185e58a7597d1d05cda8048746b23fc2ec98216
Fredrick Jones <fredrick.w.jones@gmail.com>
2015-03-10 13:40:56 -0600

    live console.log

--- public/app/views/rockClimbing/rockCtrl.js
+++ public/app/views/rockClimbing/rockCtrl.js
@@ -3,7 +3,7 @@ var app = angular.module('knotOutdoors');
 
 app.controller('rockCtrl', function($scope, rockService, locationService, uiGmapGoogleMapApi, crags, center) {
 	$scope.crags = crags;
-	// console.log(crags);
+	console.log(crags);
 	$scope.map = center;
 	// console.log(center);
 	$scope.userPin = {
@@ -15,10 +15,6 @@ app.controller('rockCtrl', function($scope, rockService, locationService, uiGmap
 		url: 'images/user-marker.png'
 	};
 	
-	var updateCrags = function(crags) {
-		$scope.crags = crags;
-	};
-
 	$scope.windowOptions = {
 		visible: false
 	};
